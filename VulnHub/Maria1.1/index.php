1dd253aafefb1a8d1698f9b7ba4b0f3730aa768c
Vishal Biswas <34629291+CyberKnight00@users.noreply.github.com>
2019-09-01 18:03:01 +0530

    Add files via upload

---
+++ VulnHub/Maria1.1/index.php
@@ -0,0 +1,64 @@
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+.tg .tg-yw4l{vertical-align:top}
+</style>
+<table class="tg">
+  <tr>
+    <th class="tg-yw4l"><b>Prisoner's name</b><br></th>
+    <th class="tg-yw4l"><b>Passkey</b></th>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Balin<br></td>
+    <td class="tg-yw4l">c2d8960157fc8540f6d5d66594e165e0</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Oin</td>
+    <td class="tg-yw4l">727a279d913fba677c490102b135e51e</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Ori</td>
+    <td class="tg-yw4l">8c3c3152a5c64ffb683d78efc3520114</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Maeglin</td>
+    <td class="tg-yw4l">6ba94d6322f53f30aca4f34960203703</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Fundin</td>
+    <td class="tg-yw4l">c789ec9fae1cd07adfc02930a39486a1</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Nain</td>
+    <td class="tg-yw4l">fec21f5c7dcf8e5e54537cfda92df5fe</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Dain</td>
+    <td class="tg-yw4l">6a113db1fd25c5501ec3a5936d817c29</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Thrain</td>
+    <td class="tg-yw4l">7db5040c351237e8332bfbba757a1019</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">Telchar</td>
+    <td class="tg-yw4l">dd272382909a4f51163c77da6356cc6f</td>
+  </tr>
+</table>
+
+<!--
+
+6MAp84
+bQkChe
+HnqeN4
+e5ad5s
+g9Wxv7
+HCCsxP
+cC5nTr
+h8spZR
+tb9AWe
+
+MD5(MD5(Password).Salt)
+
+-->
